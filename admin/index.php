d0233565571e77dd3cb40f90a7c974070272baf0
amrkives <115519872+amrkives@users.noreply.github.com>
2022-10-14 18:21:21 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -151,7 +151,7 @@ include "topheader.php";
 
            <form class="form-inline" method="post" action="generate_pdf.php">
 <button type="submit" id="pdf" name="generate_pdf" class="btn btn-primary"><i class="fa fa-pdf" aria-hidden="true"></i>
-Generate PDF</button>
+Generate PDF For User</button>
 </form>
            
             
